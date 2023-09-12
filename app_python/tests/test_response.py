a0aa0041f00e8ef9452ef2c64a7f5cc6705a61cf
Asem Abdelhady <asemshawkey@gmail.com>
2023-09-12 17:22:03 +0300

    Revert "Add: python & rust web_servers to load moscow's time"

--- app_python/tests/test_response.py
+++
@@ -1,9 +0,0 @@
-
-from main import app
-
-
-def test_response():
-    with app.test_client() as test_client:
-        response = test_client.get("/")
-        assert response.status_code == 200
-        assert b"time" in response.data
